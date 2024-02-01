474c6e72a5321d96bf0953c184984f9fb795ae00
xdchmlr <darren.courtney@landregistry.gov.uk>
2024-02-01 13:27:51 +0000

    v2.9 update

--- app/routes.js
+++ app/routes.js
@@ -108,4 +108,73 @@ router.post('/s2/add-attorney-name-2', function (req, res) {
 
 
 
+// v2.9
+
+// Save transferor 1 name for v2s1 (e.g., Jane Jenkins)
+
+router.post('/save-transferor-name-v2s1-1', (req, res) => {
+  const name = `${req.body.forenames} ${req.body.surname}`;
+  console.log(name); // add this
+  req.session.data['transferorV2s1Name'] = name;
+  res.redirect('/v2s1/select-capacity-at-ap-1');  
+});
+
+
+// Save capacity for transferor 1 for v2s1
+router.post('/save-transferor-capacity-v2s1-1', (req, res) => {
+    var capacity = req.body.capacity;
+    req.session.data['transferor-capacity-v2s1-1'] = capacity;
+    res.redirect('/v2s1/confirm-acting-for');
+});
+
+
+// Save transferor 2 name for v2s1 (e.g., Duncan James)
+router.post('/save-transferor-name-v2s1-2', (req, res) => {
+    const transferorName = `${req.body.forenames} ${req.body.surname}`;
+    req.session.data['transferor-name-v2s1-2'] = transferorName;
+    res.redirect('/v2s1/select-capacity-at-ap-2');
+});
+
+// Save capacity for transferor 2 for v2s1
+router.post('/save-capacity-v2s1-2', (req, res) => {
+    const capacity = req.body.capacity;
+    req.session.data['capacity-v2s1-2'] = capacity;
+    res.redirect('/v2s1/confirm-acting-for-2');
+});
+
+// Amend unique trustee name
+router.post('/v2s1/amend-unique-trustee-name', function (req, res) {
+  var forenames = req.body.forenames;
+  var surname = req.body.surname;
+  req.session.data['transferorNameV2-1-2'] = forenames + ' ' + surname;
+  res.redirect('/v2s1/review-transferor-details-tor-1-mat-1');
+});
+
+// * Scenario - 2 *
+
+// Save transferor 1
+router.post('/save-new-proprietor-v2', (req, res) => {
+  const name = req.body.forenames + ' ' + req.body.surname;
+  req.session.data['newProprietorName2'] = name;
+  res.redirect('/v2s2/review-transferor-details-mat-1');
+
+});
+
+// Save transferor 2
+router.post('/save-transferor-v2-2-2', (req, res) => {
+    var transferorName = req.body.forenames + ' ' + req.body.surname;
+    req.session.data['transferorNameV2-2-2'] = transferorName;
+    res.redirect('/v2s2/select-capacity-np-at-ap-1'); // Ensure this path is unique for transferor 2
+});
+
+// Add attorney name 1
+router.post('/v2s2/add-attorney-name-1', function (req, res) {
+  var forenames = req.body.forenames;
+  var surname = req.body.surname;
+  req.session.data['attorneyName-v2'] = forenames + ' ' + surname;
+  res.redirect('/v2s2/review-transferor-details-mat-2-attorney-1');
+});
+
+
+
 module.exports = router;
